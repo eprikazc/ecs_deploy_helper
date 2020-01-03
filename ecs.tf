f9687044ca005a0aaa371ded873449dc35f7c8f5
Eugene Prikazchikov <eprikazc@gmail.com>
2020-01-03 21:22:43 +0300

    Move variables to separate file

--- ecs.tf
+++ ecs.tf
@@ -1,13 +1,3 @@
-variable "ecr_repo_host" {
-  type = string
-  default = "381040904611.dkr.ecr.eu-central-1.amazonaws.com"
-}
-
-variable "splunk_repo_name" {
-  type = string
-  default = "splunk_server"
-}
-
 resource "aws_ecr_repository" "splunk_server" {
   name = var.splunk_repo_name
   lifecycle {
---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "ecr_repo_host" {
+  type = string
+  default = "381040904611.dkr.ecr.eu-central-1.amazonaws.com"
+}
+
+variable "splunk_repo_name" {
+  type = string
+  default = "splunk_server"
+}
