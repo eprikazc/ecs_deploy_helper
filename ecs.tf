f7ead679b8dc8710398c71ee712a21244b049c65
Eugene Prikazchikov <eprikazc@gmail.com>
2020-01-03 21:20:25 +0300

    Remove quotes from variable type declarations

--- ecs.tf
+++ ecs.tf
@@ -1,10 +1,10 @@
 variable "ecr_repo_host" {
-  type = "string"
+  type = string
   default = "381040904611.dkr.ecr.eu-central-1.amazonaws.com"
 }
 
 variable "splunk_repo_name" {
-  type = "string"
+  type = string
   default = "splunk_server"
 }
 
